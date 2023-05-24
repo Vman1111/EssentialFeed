4a3f15a65f2dfcbc30f1f75a1edc61888dd6d22a
Vytautas Sapranavicius <vytautas.sapranavicius@verizonconnect.com>
2023-05-24 19:23:54 +0100

    Create the FeedItem modal

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Vytautas Sapranavicius on 24/05/2023.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
