8f165637abe191cab531d55c1bf5480117aa7c65
Vman1111 <visap@protonmail.com>
2023-12-03 08:21:12 +0000

    Add `EssentailFeediOS` framework (prod and test) targets for iOS platform-specific components

---
+++ Essential Feed/EssentialFeediOSTests/EssentialFeediOSTests.swift
@@ -0,0 +1,15 @@
+//
+//  EssentialFeediOSTests.swift
+//  EssentialFeediOSTests
+//
+//  Created by Vytautas Sapranavicius on 03/12/2023.
+//
+
+import XCTest
+@testable import EssentialFeediOS
+
+final class EssentialFeediOSTests: XCTestCase {
+
+
+    
+}
